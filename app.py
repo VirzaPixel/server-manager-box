46eefd6336411f97e74a3dc56e8691a363415f00
VirzaPixezz <virzarizky151@gmail.com>
2026-06-17 01:06:28 +0700

    07 Fix Workflows bug

--- app.py
+++ app.py
@@ -185,5 +185,6 @@ def app_header(response):
     response.headers['ngrok-skip-browser-warning'] = 'true'
     return response
 
+# configures the debug True for workflows working
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
